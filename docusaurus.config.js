e578d3af02c5673e423305c45847d075514171f6
slavamorozov <morozikchetkiu2@gmail.com>
2024-09-02 22:05:58 +0300

    push changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://moroziksas.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/aboba/',
   deploymentBranch: 'gh-pages',
 
   // GitHub pages deployment config.
